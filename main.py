202415d68845cc384b125e15cf0fdec4729ec3b0
sebciks <sebastians.ivanovskis@inbox.lv>
2023-11-09 10:35:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ window = tk.Tk()
 label = tk.Label(text="Some test text")
 label.pack()
 window.mainloop()
+while y > 0:
+ay = -g
+y += vy * dt # use old vy to calculate new y
+vy += ay * dt # use old ay to calculate new vy
+t += dt
