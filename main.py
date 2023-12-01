9fc32f78ccfae3e114702509aff2e8c842ff7673
J-liepins <107466666+J-liepins@users.noreply.github.com>
2023-12-01 12:41:43 +0200

    drag drop functionality

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 from screen import Screen
+import math
 
-screen = Screen()
+dragObj = False
+def clickCallback(event):
+    global dragObj
+    distance = math.sqrt((event.x - x)**2 + (event.y - y)**2)
+    if distance <= 60:
+        dragObj = True
+
+def releaseCallback(event):
+    global dragObj
+    global physics
+    dragObj = False
+    physics = True
+    global vy
+    vy = 0
+
+def dragCallback(event):
+    global dragObj
+    global x
+    global y
+    global physics
+    if dragObj:
+        physics = False
+        x = event.x
+        y = event.y
+    else:
+        physics = True
+
+screen = Screen(clickCallback, releaseCallback, dragCallback)
 x = screen.SCR_WIDTH/2
 y = screen.SCR_HEIGHT/2
+physics = True
 vx = 3
 vy = 1
 while True:
-    x += vx
-    y += vy 
-    vy+=screen.FRAME_TIME*9.81
-    if (y >= screen.SCR_HEIGHT and vy>0) or (y <= 0 and vy<0):
-        vy /= -1.2
-    if (x >= screen.SCR_WIDTH and vx>0) or (x <= 0 and vx<0):
-        vx *= -1
+    if physics:
+        x += vx
+        y += vy
+        vy+=screen.FRAME_TIME*9.81
+        if (y >= screen.SCR_HEIGHT and vy>0) or (y <= 0 and vy<0):
+            vy /= -1.2
+        if (x >= screen.SCR_WIDTH and vx>0) or (x <= 0 and vx<0):
+            vx *= -1
     # screen.drawLine([x - 5,y - 5, x + 5, y + 5])
     screen.drawCircle(x, y, 20, outline='black', fill=None, width = 5)
-
+    
     # Canvas outline
     screen.drawLine([0, 0, Screen.SCR_WIDTH, 0, Screen.SCR_WIDTH, Screen.SCR_HEIGHT, 0, Screen.SCR_HEIGHT, 0, 0])
     # Draw stuff and delete old lines
